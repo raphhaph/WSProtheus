23bafaa4d1a72fe82e0057b41ff4805118b573e4
rbercocano@gmail.com <rbercocano@gmail.com>
2016-06-10 09:28:50 -0400

    a

---
+++ TMF.Protheus_HRP.Domain.RequestResponse/Models/DadoGenerico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMF.Protheus_HRP.Domain.RequestResponse.Models
+{
+    public class DadoGenerico
+   {
+       [DataMember]
+       public string Codigo { get; set; }
+       [DataMember]
+       public string Descricao { get; set; }
+
+   }
+}
