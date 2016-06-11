579ee2e988573c75f82f5bb6f7a32c487df29619
rbercocano <rbercocano@gmail.com>
2016-06-11 12:58:16 -0400

    aa

---
+++ TMF.Protheus_HRP.Domain.DTO/TiposSX5.cs
@@ -0,0 +1,20 @@
+﻿namespace TMF.Protheus_HRP.Domain.DTO
+{
+    public enum TiposSX5
+    {
+        UF = 12,
+        VinculoRaiz = 25,
+        GrauInstrucao = 26,
+        TipoDemissao = 27,
+        TipoFuncionario = 28,
+        TipoAfastamento = 30,
+        SituacaoFuncionario = 31,
+        CodigosAdicionalTempoServico = 32,
+        EstadoCivil = 33,
+        Nacionalidade = 34,
+        TipoAdmissao = 38,
+        Recebimento = 40,
+        AlteracaoSalarial = 41,
+        OrgaoRG = 64
+    }
+}
